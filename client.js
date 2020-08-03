80ae8bdd9a29fcedcd2cc390f652e7e0d65ab604
cyw <18604469853@163.com>
2020-08-03 23:32:34 +0800

    toy browser init

---
+++ client.js
@@ -0,0 +1,12 @@
+const net = require("net");
+net.connect({
+    port: 8088,
+    onread: {
+        // Reuses a 4KiB Buffer for every read from the socket.
+        buffer: Buffer.alloc(4 * 1024),
+        callback: function(nread, buf) {
+            // Received data is available in `buf` from 0 to `nread`.
+            console.log(buf.toString('utf8', 0, nread));
+        }
+    }
+});
